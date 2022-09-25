d76145316aad84d2b7c8740bf35516dbeeeefc16
Francisco Antonio Paiva <fcooantoniops@gmail.com>
2022-09-25 15:28:42 -0300

    POST/choice ready

---
+++ src/controllers/choiceController.js
@@ -0,0 +1,39 @@
+import { ObjectId } from "mongodb";
+import db from "../database.js";
+import dayjs from "dayjs";
+import isSameOrBefore from "dayjs/plugin/isSameOrBefore.js";
+
+export async function createChoice(req, res) {
+  const { title, poolId } = req.body;
+  const now = dayjs().format("YYYY-MM-DD HH:mm");
+
+  try {
+    const isAPool = await db
+      .collection("pools")
+      .findOne({ _id: new ObjectId(poolId) });
+    if (!isAPool) {
+      return res.sendStatus(404);
+    }
+
+    const usedTitle = await db.collection("choices").findOne({ title });
+    if (usedTitle) {
+      return res.sendStatus(409);
+    }
+
+    dayjs.extend(isSameOrBefore);
+    const expireDate = await db
+      .collection("pools")
+      .findOne({ _id: new ObjectId(poolId) });
+    if (dayjs(expireDate.expireAt).isSameOrBefore(dayjs(now))) {
+      return res.sendStatus(403);
+    }
+
+    await db
+      .collection("choices")
+      .insertOne({ title, poolId: new ObjectId(poolId) });
+    res.sendStatus(201);
+  } catch (error) {
+    console.error(error.message);
+    res.sendStatus(500);
+  }
+}
